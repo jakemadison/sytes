0f2766eda37ed18cfaa362e2e69f3eda5a361fa3
jakemadison <jakemadison@gmail.com>
2015-03-29 09:31:16 -0700

    instagram auth done.  jeeeebs.

--- syte/syte_settings.py
+++ syte/syte_settings.py
@@ -53,12 +53,12 @@ DRIBBBLE_API_URL = 'http://api.dribbble.com/players/'
 #Instagram Integration
 INSTAGRAM_INTEGRATION_ENABLED = True
 INSTAGRAM_API_URL = 'https://api.instagram.com/v1/'
-INSTAGRAM_ACCESS_TOKEN = '[ENTER INSTAGRAM ACCESS TOKEN HERE, SEE INSTAGRAM SETUP INSTRUCTIONS]'
-INSTAGRAM_USER_ID = '[ENTER INSTAGRAM USER_ID HERE, SEE INSTAGRAM SETUP INSTRUCTIONS]'
+INSTAGRAM_ACCESS_TOKEN = key_config.instagram_key_dict['token']
+INSTAGRAM_USER_ID = key_config.instagram_key_dict['user_id']
 
 INSTAGRAM_OAUTH_ENABLED = True
-INSTAGRAM_CLIENT_ID = '[ENTER INSTAGRAM CLIENT_ID HERE, SEE INSTAGRAM SETUP INSTRUCTIONS]'
-INSTAGRAM_CLIENT_SECRET = '[ENTER INSTAGRAM CLIENT_SECRET HERE, SEE INSTAGRAM SETUP INSTRUCTIONS]'
+INSTAGRAM_CLIENT_ID = key_config.instagram_key_dict['client_id']
+INSTAGRAM_CLIENT_SECRET = key_config.instagram_key_dict['client_secret']
 INSTAGRAM_OAUTH_AUTHORIZE_URL = 'https://api.instagram.com/oauth/authorize'
 INSTAGRAM_OAUTH_ACCESS_TOKEN_URL = 'https://api.instagram.com/oauth/access_token'
 
